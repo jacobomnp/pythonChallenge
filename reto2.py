2a84f826f47d470a683847a954e4ac530589cf08
Jacobomnp <jacobomnp@gmail.com>
2023-03-16 18:26:00 -0600

    respuesta reto2

---
+++ reto2.py
@@ -0,0 +1,64 @@
+''''/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */'''
+diccionario = {
+    "A":"4",
+    "B":"I3",
+    "C":"[",
+    "D":")",
+    "E":"3",
+    "F":"|=",
+    "G":"&",
+    "H":"#",
+    "I":"1",
+    "J":",_|",
+    "K":">|",
+    "L":"1",
+    "M":"JVI",
+    "N":"^/",
+    "O":"0",
+    "P":"|*",
+    "Q":"(_,)",
+    "R":"I2",
+    "S":"5",
+    "T":"7",
+    "U":"(_)",
+    "V":"|/",
+    "W":"\/\/",
+    "X":"><",
+    "Y":"j",
+    "Z":"2",
+    "1":"L",
+    "2":"R",
+    "3":"E",
+    "4":"A",
+    "5":"S",
+    "6":"b",
+    "7":"T",
+    "8":"B",
+    "9":"g",
+    "0":"o",
+}
+
+def lenguajeHacker (texto):
+    texto_convertido = ""
+
+    texto = texto.upper()
+
+    for letra in texto:
+        if letra in diccionario:
+            letra = diccionario[letra]
+            texto_convertido += letra
+        else:
+            texto_convertido += letra
+
+    return texto_convertido
+        
+
+texto = input("Introduce el texto que quieras convertir: ")
+print(lenguajeHacker(texto))
